e0f3b374457e405323fc9b7798a0c01416e1a270
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-09-08 15:07:50 +0200

    Update lexer_test.v

--- src/lexer/lexer_test.v
+++ src/lexer/lexer_test.v
@@ -24,3 +24,39 @@ fn test_tokenzier() {
 	assert lexer.tokens.len == tokens.len
 	assert lexer.tokens == tokens
 }
+
+fn test_tokenzier_2() {
+	data := '[{"kind":"number_kind","value":"12"},{"kind":"plus_kind","value":"+"},{"kind":"number_kind","value":"3"},{"kind":"multiply_kind","value":"*"},{"kind":"number_kind","value":"1"},{"kind":"semi_colon_kind","value":";"}]'
+	tokens := json.decode([]Token, data)!
+
+	path := 'examples/example2.c'
+	mut lexer := create_lexer(path)!
+
+	lexer.tokenize()
+	assert lexer.tokens.len == tokens.len
+	assert lexer.tokens == tokens
+}
+
+fn test_tokenzier_3() {
+	data := '[{"kind":"number_kind","value":"12"},{"kind":"multiply_kind","value":"*"},{"kind":"number_kind","value":"3"},{"kind":"plus_kind","value":"+"},{"kind":"number_kind","value":"1"},{"kind":"semi_colon_kind","value":";"}]'
+	tokens := json.decode([]Token, data)!
+
+	path := 'examples/example3.c'
+	mut lexer := create_lexer(path)!
+
+	lexer.tokenize()
+	assert lexer.tokens.len == tokens.len
+	assert lexer.tokens == tokens
+}
+
+fn test_tokenzier_4() {
+	data := '[{"kind":"int_kind","value":"int"},{"kind":"identifier_kind","value":"a"},{"kind":"assignment_kind","value":"="},{"kind":"number_kind","value":"5"},{"kind":"semi_colon_kind","value":";"},{"kind":"int_kind","value":"int"},{"kind":"identifier_kind","value":"b"},{"kind":"assignment_kind","value":"="},{"kind":"identifier_kind","value":"a"},{"kind":"multiply_kind","value":"*"},{"kind":"number_kind","value":"10"},{"kind":"semi_colon_kind","value":";"},{"kind":"bool_kind","value":"bool"},{"kind":"identifier_kind","value":"is_foo"},{"kind":"assignment_kind","value":"="},{"kind":"identifier_kind","value":"a"},{"kind":"greater_than_kind","value":">"},{"kind":"identifier_kind","value":"b"},{"kind":"semi_colon_kind","value":";"}]'
+	tokens := json.decode([]Token, data)!
+
+	path := 'examples/example4.c'
+	mut lexer := create_lexer(path)!
+
+	lexer.tokenize()
+	assert lexer.tokens.len == tokens.len
+	assert lexer.tokens == tokens
+}
